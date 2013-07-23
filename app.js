cd3f444f71adfd3a8862f56ea82e2194c7235d08
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2013-07-23 12:45:33 +0200

    Intial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('<h1>Hello world!</h1>');
+}).listen(process.env.PORT || 8001);
